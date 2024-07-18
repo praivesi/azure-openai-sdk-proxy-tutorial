32599cee9561d71e8cdf5c24956558513ec18abe
praivesi <praivesi@gmail.com>
2024-07-18 18:43:52 +0900

    feat: Pizza Order 기능 추가

---
+++ BlazingPizza/Controllers/SpecialsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Data;
+
+namespace BlazingPizza.Controllers;
+
+[Route("specials")]
+[ApiController]
+public class SpecialsController : Controller
+{
+    private readonly PizzaStoreContext _db;
+
+    public SpecialsController(PizzaStoreContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+    {
+        var data = (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+
+        Console.WriteLine("data: " + data);
+
+        return data;
+
+        // return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+    }
+}
+
--- BlazingPizza/Program.cs
+++ BlazingPizza/Program.cs
@@ -1,10 +1,16 @@
 using BlazingPizza.Data;
+using BlazingPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<PizzaService>();
+
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
@@ -19,5 +25,24 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// DB
+
+Console.WriteLine("before scopeFactory");
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        Console.WriteLine("before SeedData initialize");
+
+        SeedData.Initialize(db);
+
+        Console.WriteLine("after SeedData initialize");
+    }
+}
+Console.WriteLine("after scopeFactory");
 
 app.Run();
